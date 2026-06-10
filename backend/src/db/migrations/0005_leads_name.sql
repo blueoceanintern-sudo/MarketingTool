62172a8ed8112e13ca387c752b6f3f5aa05dd7a0
Jachin-Khoo <jachinkhoo@gmail.com>
2026-06-10 10:16:41 +0700

    Name only, no first/last name.

--- backend/src/db/migrations/0005_leads_name.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "leads" ADD COLUMN "name" text;--> statement-breakpoint
-UPDATE "leads" SET "name" = TRIM(CONCAT_WS(' ', "first_name", "last_name")) WHERE "first_name" IS NOT NULL OR "last_name" IS NOT NULL;--> statement-breakpoint
-ALTER TABLE "leads" DROP COLUMN IF EXISTS "first_name";--> statement-breakpoint
-ALTER TABLE "leads" DROP COLUMN IF EXISTS "last_name";
