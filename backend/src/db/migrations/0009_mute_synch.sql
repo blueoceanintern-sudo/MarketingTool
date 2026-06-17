e303a0bce3b795a71e18572c4292312d1604a495
Jachin-Khoo <jachinkhoo@gmail.com>
2026-06-17 10:41:35 +0700

    migration files fix

--- backend/src/db/migrations/0009_mute_synch.sql
+++ backend/src/db/migrations/0009_mute_synch.sql
@@ -1,3 +1,4 @@
 ALTER TABLE "leads" ADD COLUMN "name" text;--> statement-breakpoint
+UPDATE "leads" SET "name" = NULLIF(TRIM(CONCAT_WS(' ', "first_name", "last_name")), '');--> statement-breakpoint
 ALTER TABLE "leads" DROP COLUMN "first_name";--> statement-breakpoint
 ALTER TABLE "leads" DROP COLUMN "last_name";
