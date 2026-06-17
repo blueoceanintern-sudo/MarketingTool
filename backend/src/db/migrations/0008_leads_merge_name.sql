60c8047a2900c8ec6e8e67d89c509c3d8915a3d8
Jachin-Khoo <jachinkhoo@gmail.com>
2026-06-17 10:41:35 +0700

    migration files fix

--- backend/src/db/migrations/0008_leads_merge_name.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "leads" ADD COLUMN "name" text;--> statement-breakpoint
-UPDATE "leads" SET "name" = NULLIF(TRIM(CONCAT_WS(' ', "first_name", "last_name")), '');--> statement-breakpoint
-ALTER TABLE "leads" DROP COLUMN "first_name";--> statement-breakpoint
-ALTER TABLE "leads" DROP COLUMN "last_name";
